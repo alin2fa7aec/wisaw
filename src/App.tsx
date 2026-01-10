7dedc29a8bb003db3316b281878c4c5e4c9bd0dc
alin <alin2fa7aec@gmail.com>
2026-01-10 17:20:46 +0900

    feat: MynaUI から適当にコンポーネントを import して足がかりとする

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Card, CardContent } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = () => {
+	return (
+		<div className="flex flex-row gap-4">
+			<Card className="shadow-none">
+				<CardContent className="pt-6">
+					<Textarea id="name" placeholder="Enter your name" />
+					<p className="text-xs font-medium text-destructive">
+						This field is required
+					</p>
+				</CardContent>
+			</Card>
+			<Card className="shadow-none">
+				<CardContent className="pt-6">
+					<p className="text-xs font-extrabold">うみみゃあ！！</p>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+	return (
+		<>
+			<div>
+				<Container />
+			</div>
+		</>
+	);
+};
 
-export default App
+export default App;
